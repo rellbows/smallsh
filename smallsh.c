3d9c0c38dec83e8a5d5c2f57438e76dd1cc64489
ellisry <ellisry@oregonstate.edu>
2019-02-23 10:06:26 -0800

    Initial commit

---
+++ smallsh.c
@@ -0,0 +1,10 @@
+// Prog. Name: smallsh
+// Author: Ryan Ellis
+// Class: CS344 - Operating Systems
+// Description: Simple C script that creates a shell that can be used to 
+// make system calls to the OS.
+
+int main(){
+	
+	return 0;
+}
